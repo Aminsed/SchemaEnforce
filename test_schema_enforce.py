5b3a6985a47dc65c20482ee4c3bf405e6f539ed5
Aminsed <35748194+Aminsed@users.noreply.github.com>
2023-02-24 21:12:11 -0500

    added testing and tested to check all passes

--- test_schema_enforce.py
+++ test_schema_enforce.py
@@ -1,54 +1,43 @@
-import data_type
 import unittest
 import pandas as pd
 import json
-from main import SchemaEnforce
-
+import os
+from schema_enforce import SchemaEnforce
 class TestSchemaEnforce(unittest.TestCase):
-    
+
     def setUp(self):
-        self.df = pd.DataFrame({'a': [1,2,3], 'b': ['a', 'b', 'c']})
-        self.schema_file = 'schema.json'
-        self.pattern_file = 'pattern.json'
-        self.schema_enforcer = SchemaEnforce(self.df, self.schema_file, self.pattern_file)
+        self.df = pd.DataFrame({
+            'Name': ['Alice', 'Bob', 'Charlie'],
+            'Age': [25, 35, 45],
+            'Gender': ['Female', 'Male', 'Male'],
+            'Salary': [50000, 60000, 70000],
+            'Start Date': ['2022-01-01', '2021-01-01', '2020-01-01']
+        })
+        self.schema_file = 'schema_test.json'
+        self.log_file = 'schema_enforce_test.log'
+        self.schema_enforcer = SchemaEnforce(self.df, self.schema_file, self.log_file)
+
+    def tearDown(self):
+        os.remove(self.schema_file)
+        os.remove(self.log_file)
 
     def test_export_schema_to_json(self):
-        self.schema_enforcer.export_schema_to_json()
-        with open('schema.json', 'r') as f:
-            output = json.load(f)
-        self.assertEqual(output['a'], 'int64')
-        self.assertEqual(output['b'], 'object')
-        
-    def test_verify_schema(self):
-        self.schema_enforcer.verify_schema()
+        self.schema_enforcer.export_schema_to_json(self.schema_file)
         with open(self.schema_file, 'r') as f:
             schema = json.load(f)
-        self.assertEqual(str(self.df.dtypes[0]), schema['a'])
-        self.assertEqual(str(self.df.dtypes[1]), schema['b'])
-        
-    def test_export_pattern_to_json(self):
-        self.schema_enforcer.export_pattern_to_json()
-        with open('learned_pattern.json', 'r') as f:
-            output = json.load(f)
-        self.assertEqual(output['a']['mean'], 2)
-        self.assertEqual(output['a']['median'], 2)
-        self.assertEqual(output['a']['mode'], 0)
-        self.assertEqual(output['a']['min'], 1)
-        self.assertEqual(output['a']['max'], 3)
-        self.assertEqual(output['b'][0], 'a')
-        self.assertEqual(output['b'][1], 'b')
-        self.assertEqual(output['b'][2], 'c')
-        
-    def test_verify_pattern(self):
-        self.schema_enforcer.verify_pattern()
-        with open(self.pattern_file, 'r') as f:
-            pattern = json.load(f)
-        self.assertEqual(self.df['a'].mean(), pattern['a']['mean'])
-        self.assertEqual(self.df['a'].median(), pattern['a']['median'])
-        self.assertEqual(self.df['a'].mode(), pattern['a']['mode'])
-        self.assertEqual(self.df['a'].min(), pattern['a']['min'])
-        self.assertEqual(self.df['a'].max(), pattern['a']['max'])
-        self.assertEqual(self.df['b'].str.extract(r'(\w+)'), pattern['b'])
+        self.assertDictEqual(schema, {'Name': 'object', 'Age': 'int64', 'Gender': 'object', 'Salary': 'int64', 'Start Date': 'object'})
+
+    def test_verify_schema_success(self):
+        self.schema_enforcer.export_schema_to_json(self.schema_file)
+        self.schema_enforcer.verify_schema(self.schema_file)
+        with open(self.log_file, 'r') as f:
+            logs = f.read()
+        self.assertIn('Schema matches column Name.', logs)
+        self.assertIn('Schema matches column Age.', logs)
+        self.assertIn('Schema matches column Gender.', logs)
+        self.assertIn('Schema matches column Salary.', logs)
+        self.assertIn('Schema matches column Start Date.', logs)
+
 
 if __name__ == '__main__':
     unittest.main()
